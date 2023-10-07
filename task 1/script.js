7914dc445b2cb1bb616c25f105b1fcaf958a4447
unknown <nandanjain04@gmail.com>
2023-10-07 21:01:09 +0530

    Added Main Banner (Hero Section)

--- task 1/script.js
+++ task 1/script.js
@@ -1,2 +1,10 @@
 const currentYear = new Date().getUTCFullYear();
 document.getElementById("current-year").innerHTML = currentYear;
+const navBar = document.getElementById("nav");
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+        navBar.classList.add("scrolled-nav");
+    } else {
+        navBar.classList.remove("scrolled-nav");
+    }
+});
